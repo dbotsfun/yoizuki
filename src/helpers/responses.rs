6f8d655a93e975a4c404b2d69b02ac6c39be594a
Chikof <chiko@envs.net>
2024-05-13 16:57:28 +0100

    feat: worker-rs implmentation with axum

--- src/helpers/responses.rs
+++ src/helpers/responses.rs
@@ -3,6 +3,6 @@ use serde::{Deserialize, Serialize};
 /// Custom Response
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CustomResponse {
-    /// Message to be sent
-    pub message: String,
+	/// Message to be sent
+	pub message: String,
 }
